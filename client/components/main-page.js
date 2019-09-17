f5c2627e4661699d5cc4a790bdf35e8ff7a3a1b2
Dominic Sciara <dominic.sciara@gmail.com>
2019-09-17 16:16:45 -0400

    fixed bugs

--- client/components/main-page.js
+++ client/components/main-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import {Login, Signup} from './auth-form'
 import UserHome from './user-home'
-import BoardList from './board-all'
+import BoardList from './board-list'
 import BoardSingle from './board-single'
 
 const MainPage = () => {
