32f0c1c1da1dcf04db1ef7f2ba2d2635fee4b349
Dominic Sciara <dominic.sciara@gmail.com>
2019-09-17 15:06:27 -0400

    fixed bug with board component and made order component

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import board from './board'
+import order from './order'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, board, order})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +14,5 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './board'
+export * from './order'
