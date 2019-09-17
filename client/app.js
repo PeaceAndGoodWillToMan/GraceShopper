94fa9c8e667bf6f13c4b49f0a7055489088231c6
Robert Schnek <robert.schnek@gmail.com>
2019-09-17 12:57:46 -0400

    Added logic for seeing main page rather than login page and added buttons to navbar

--- client/app.js
+++ client/app.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import {Navbar, UserHome} from './components'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes />
+      <UserHome />
     </div>
   )
 }
