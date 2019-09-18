54260ef4bb583ddb962bde074e764b529d97ee0f
FergalCG <https://FergalCG@github.com>
2019-09-17 21:15:14 -0400

    fixed routes, log-in persistence, and Oauth

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import Routes from './routes'
 
 // establishes socket connection
 import './socket'
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <App />
+      <Routes />
     </Router>
   </Provider>,
   document.getElementById('app')
--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
+import BoardList from './components/board-all'
 import {me} from './store'
 
 /**
@@ -14,22 +15,13 @@ class Routes extends Component {
   }
 
   render() {
-    const {isLoggedIn} = this.props
-
     return (
-      <Switch>
-        {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
-      </Switch>
+      <div>
+        <Route exact path="/home" component={UserHome} />
+        <Route exact path="/boards" component={BoardList} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </div>
     )
   }
 }
@@ -37,13 +29,13 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+// const mapState = state => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+//     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+//     isLoggedIn: !!state.user.id
+//   }
+// }
 
 const mapDispatch = dispatch => {
   return {
@@ -55,12 +47,11 @@ const mapDispatch = dispatch => {
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(null, mapDispatch)(Routes))
 
 /**
  * PROP TYPES
  */
 Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  loadInitialData: PropTypes.func.isRequired
 }
