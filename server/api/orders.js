43ccd7667a9219c681b3a910172b3abc51297a3b
Dominic Sciara <dominic.sciara@gmail.com>
2019-09-19 12:05:43 -0400

    routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,27 @@
 const router = require('express').Router()
-const {Order} = require('../db/models')
+const {Order, BoardOrder} = require('../db/models')
 
 router.get('/', async (req, res, next) => {
   try {
     const orders = await Order.findAll()
     res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/logout', async (req, res, next) => {
+  try {
+    const userId = req.session.passport.user
+    const order = await Order.create({userId})
+    const bo = {
+      orderId: order.id,
+      boardId: req.body.localStorage.id,
+      quantity: req.body.localStorage.quantity,
+      price: req.body.localStorage.price
+    }
+    const boardOrder = await BoardOrder.create(bo)
+    res.sendStatus(201).json(boardOrder)
   } catch (err) {
     next(err)
   }
