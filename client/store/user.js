f27f7de763ad51e2ba783368dc3501b154e1d262
FergalCG <https://FergalCG@github.com>
2019-09-24 12:20:34 -0400

    guest cart will now merge with A users old cart on login or become a new Users cart on  signup

--- client/store/user.js
+++ client/store/user.js
@@ -38,22 +38,49 @@ export const auth = (email, password, method) => async dispatch => {
   } catch (authError) {
     return dispatch(getUser({error: authError}))
   }
-
   try {
     dispatch(getUser(res.data))
     const {data} = await axios.get('/api/orders/login')
-    for (let i = 0; i < data.boards.length; i++) {
-      let key = JSON.stringify({id: data.boards[i].id})
-      let value = JSON.stringify({
-        name: data.boards[i].name,
-        imageUrl: data.boards[i].imageUrl,
-        quantity: data.boardOrder[i].quantity,
-        price: data.boards[i].price * data.boardOrder[i].quantity,
-        stock: data.boards[i].stock
-      })
-      window.localStorage.setItem(key, value)
+    if (data.boards) {
+      if (window.localStorage.length) {
+        for (let i = 0; i < data.boards.length; i++) {
+          let key = JSON.stringify({id: data.boards[i].id})
+          let value = {
+            name: data.boards[i].name,
+            imageUrl: data.boards[i].imageUrl,
+            quantity: data.boardOrder[i].quantity,
+            price: data.boards[i].price * data.boardOrder[i].quantity,
+            stock: data.boards[i].stock
+          }
+          if (window.localStorage.getItem(key)) {
+            let tempPrice = JSON.parse(window.localStorage.getItem(key)).price
+            let tempQuant = JSON.parse(window.localStorage.getItem(key))
+              .quantity
+            const newVal = JSON.stringify({
+              ...value,
+              price: value.price + tempPrice,
+              quantity: value.quantity + tempQuant
+            })
+            window.localStorage.setItem(key, newVal)
+          } else {
+            window.localStorage.setItem(key, JSON.stringify(value))
+          }
+        }
+      } else {
+        for (let i = 0; i < data.boards.length; i++) {
+          let key = JSON.stringify({id: data.boards[i].id})
+          let value = JSON.stringify({
+            name: data.boards[i].name,
+            imageUrl: data.boards[i].imageUrl,
+            quantity: data.boardOrder[i].quantity,
+            price: data.boards[i].price * data.boardOrder[i].quantity,
+            stock: data.boards[i].stock
+          })
+          window.localStorage.setItem(key, value)
+        }
+      }
+      stateChange()
     }
-    stateChange()
     history.push('/home')
   } catch (dispatchOrHistoryErr) {
     console.error(dispatchOrHistoryErr)
